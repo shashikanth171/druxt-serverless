7c21f055600199ce4e6a4b3611db69c0e93effd5
Shashikanth Reddy Palvatla <25389431+shashikanth171@users.noreply.github.com>
2022-05-20 13:20:23 +0530

    Initial commit

---
+++ nuxt/nuxt.config.js
@@ -0,0 +1,72 @@
+import opn from 'opn'
+
+const baseUrl = process.env.BASE_URL || 'http://quickstart-druxt-serverless.ddev.site'
+
+export default {
+  // Target full static build.
+  target: 'static',
+
+  // Ensure the root route is generated and crawled.
+  generate: {
+    routes: ['/']
+  },
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'quickstart-druxt-site',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    ['@nuxt/image', { domains: [baseUrl] }],
+    'druxt-site',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [],
+
+  // DruxtJS: https://druxtjs.org
+  druxt: {
+    baseUrl,
+    // Enable the API proxy.
+    proxy: { api: true },
+    // Disable deprecated Entity fields.
+    entity: { components: { fields: false }},
+    // Disable the router middleware (redirect support) in favour of serverless.
+    router: { middleware: false },
+    // Set the default theme to render Site regions.
+    site: { theme: 'bartik' },
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  },
+
+  hooks: {
+    // Open browser once build is done.
+    'build:done': () => opn('https://localhost:3000')
+  }
+}
